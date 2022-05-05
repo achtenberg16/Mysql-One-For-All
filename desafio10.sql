8dd740c75243800f95d0ec5596bfc27e4dc04591
achtenberg16 <achtenberg16@gmail.com>
2022-05-05 20:56:42 -0300

    feat: adicionado desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,7 @@
+SELECT musica_nome AS nome, count(m.musica_id) as reproducoes FROM SpotifyClone.musicas m
+INNER JOIN SpotifyClone.historico_reproducoes h
+ON m.musica_id = h.musica_id
+INNER JOIN SpotifyClone.users u
+ON u.user_id = h.user_id
+WHERE plano_id IN (1,2)
+GROUP BY musica_nome;
